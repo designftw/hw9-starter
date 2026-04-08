b84dd21cddce3cf453c5399ff50acec5f5492226
theia <theia@mit.edu>
2026-04-08 18:26:45 -0400

    update tracker code

--- tracker/index.js
+++ tracker/index.js
@@ -1,70 +1,133 @@
-import { createApp } from "vue";
+import { createApp, computed } from "vue";
 import { GraffitiLocal } from "@graffiti-garden/implementation-local";
-import { GraffitiRemote } from "@graffiti-garden/implementation-remote";
-import { GraffitiPlugin } from "@graffiti-garden/wrapper-vue";
+import { GraffitiDecentralized } from "@graffiti-garden/implementation-decentralized";
+import {
+  GraffitiPlugin,
+  useGraffiti,
+  useGraffitiSession,
+  useGraffitiDiscover,
+} from "@graffiti-garden/wrapper-vue";
 
-createApp({
-  data() {
-    return {
-      entries: [],
-    };
-  },
+function setup() {
+  const graffiti = useGraffiti();
+  const session = useGraffitiSession();
 
-  methods: {
-    async addEntry(content) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      await this.$graffiti.put(
-        {
-          value: {
-            published: Date.now(),
-            content,
-            // Feel free to add more properties here
-            // based on your personal tracker's needs!
-          },
-          allowed: [],
-          channels: [this.$graffitiSession.value],
+  // Store the tracker entries in my "tracker" folder
+  const trackerChannels = computed(() => {
+    return session.value ? [session.value.actor + "/tracker"] : [];
+  });
+
+  // This is an artificial delay for local testing async tasks
+  async function delay() {
+    await new Promise((r) => setTimeout(r, 1000));
+  }
+
+  async function addEntry(content) {
+    await delay();
+    await graffiti.post(
+      {
+        // The object's "value" can be any JSON,
+        // feel free to change these properties
+        // and add new ones as needed based on your
+        // tracker's needs
+        value: {
+          activity: "Update",
+
+          // Generate a unique ID for the entry
+          // to identifies it across updates
+          id: crypto.randomUUID(),
+
+          // Timestamps for when the tracked entry "happened"
+          // (published) and when the tracked entry was modified (updated)
+          published: Date.now(),
+          updated: Date.now(),
+
+          // Content includes any notes for the tracker entry
+          content,
         },
-        this.$graffitiSession.value,
-      );
-    },
+        // Empty allowed list => only you can see the data
+        allowed: [],
 
-    async saveEntry(entry) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      await this.$graffiti.put(entry, this.$graffitiSession.value);
-    },
+        channels: trackerChannels.value,
+      },
+      session.value,
+    );
+  }
 
-    async deleteEntry(entry) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      await this.$graffiti.delete(entry, this.$graffitiSession.value);
-    },
+  async function saveEntry(entry) {
+    await delay();
+    entry.value.updated = Date.now();
+    await graffiti.post(entry, session.value);
+  }
 
-    async refreshEntries() {
-      const entriesIterator = this.$graffiti.discover(
-        [this.$graffitiSession.value],
-        {
-          properties: {
-            value: {
+  // Discover for objects like those produced by addEntry and saveEntry
+  const { objects: updates, isFirstPoll: areUpdatesLoading } =
+    useGraffitiDiscover(
+      trackerChannels,
+      // This is a JSONSchema
+      // https://json-schema.org/docs
+      {
+        properties: {
+          value: {
+            required: ["activity", "id", "content", "published", "updated"],
+            properties: {
+              activity: { const: "Update" },
+              id: { type: "string" },
               content: { type: "string" },
               published: { type: "number" },
+              updated: { type: "number" },
             },
           },
         },
-        this.$graffitiSession.value,
-      );
+      },
+      session,
+    );
 
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  // Merge updates activities together to only
+  // take the most recent one for each ID
+  // (this makes it possible "edit" entries
+  //  even though Graffiti only allows you to modify
+  //  objects via "post" and "delete")
+  const entries = computed(() => {
+    return Object.values(
+      updates.value.reduce((acc, object) => {
+        const { id, updated } = object.value;
+        if (!acc[id] || acc[id].value.updated < updated) {
+          acc[id] = object;
+        }
+        return acc;
+      }, {}),
+    );
+  });
 
-      this.entries = [];
-      for await (const { object } of entriesIterator) {
-        this.entries.push(object);
-      }
+  // Delete will delete all the updates for a given id
+  async function deleteEntry(entry) {
+    await delay();
+    await Promise.all(
+      updates.value
+        .filter((o) => o.value.id === entry.value.id)
+        .map((o) => graffiti.delete(o, session.value)),
+    );
+  }
 
-      // Do any processing here like sorting...
-    },
-  },
-})
+  return {
+    areUpdatesLoading,
+    entries,
+    addEntry,
+    deleteEntry,
+    saveEntry,
+  };
+}
+
+createApp({ template: "#template", setup })
   .use(GraffitiPlugin, {
+    // Use this version of Graffiti for testing/developement
     graffiti: new GraffitiLocal(),
-    // graffiti: new GraffitiRemote(),
+
+    // Use this version for production
+    // You will actually login and your data will be stored remotely
+    // which will allow it to sync across devices
+    //
+    // graffiti: new GraffitiDecentralized(),
   })
   .mount("#app");
